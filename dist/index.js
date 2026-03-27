bcf12c0d482517f3237bfa29c5f5cdb5f8dbbd2f
AIyer <abhilashsiyer@gmail.com>
2026-03-27 12:07:18 +1100

    removed source maps

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 4914:
@@ -36208,4 +36208,3 @@ module.exports = parseParams
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
